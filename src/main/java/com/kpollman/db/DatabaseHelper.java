8f0a2cbd31e909c3887f863f0a0209515b924c65
Amal Krishna <amalkrishnaprasad4@gmail.com>
2026-04-01 22:26:10 +0530

    database changes

--- src/main/java/com/kpollman/db/DatabaseHelper.java
+++ src/main/java/com/kpollman/db/DatabaseHelper.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseHelper {
-    private static final String URL = "";
-    private static final String USER = "";
+    private static final String URL ="jdbc:mysql://mysql-2276d50ckpolljava26.h.aivencloud.com:24584/kpollman2026useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true&connectTimeout=10000&socketTimeout=10000";
+    private static final String USER = "avnadmin";
     private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
