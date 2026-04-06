6eba89830823d3ef62811eb66ac0af122ce999f7
gokul-kri-shna <gogulkrishna739@gmail.com>
2026-04-06 10:33:26 +0530

    all functionalities fixed

--- src/main/java/com/kpollman/db/TestDBConnection.java
+++ src/main/java/com/kpollman/db/TestDBConnection.java
@@ -9,7 +9,7 @@ public class TestDBConnection {
     public static void main(String[] args) {
         String url = "jdbc:mysql://mysql-2276d50ckpolljava26.h.aivencloud.com:24584/kpollman2026?useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true&connectTimeout=10000&socketTimeout=10000";
         String user = "avnadmin";
-        String password = ""; // User should replace this manually for testing
+        String password = "YOUR_PASSWORD_HERE"; // User should replace this manually for testing
 
         System.out.println("Attempting to connect to: " + url);
         
